e3c3eea91b11feff1447d54f527dd3dbb9bd5121
YeonJangL <lej32552002@gmail.com>
2024-07-19 17:36:30 +0900

    AR-82 과일 랜덤 생성 수정중
    랜덤인덱스 생성함 (DataManager에 과일 리스트에 접근해서 개수 파악 하여 사용)
    ↓
    인덱스 값중 두개를 스택에 저장해둠
    ↓
    0번째는 바로 던져질 과일
    ↓
    1번째는 다음 과일 화면에 출력할 과일
    ↓
    원본 과일에 대한 레벨 정보랑 합쳐지는 정보
    ↓
    던져질 과일은 화면 하단의 과일 위치에 인스탄티에이트를 이용하여 생성
    ↓
    다음 과일 화면에 출력할 과일은 UI 다음 과일 출력 위치에 인스탄티에이트를 이용하여 생성

--- Assets/Scripts/FruitRandomSpawn.cs
+++ Assets/Scripts/FruitRandomSpawn.cs
@@ -5,29 +5,16 @@ using UnityEngine;
 
 public class FruitRandomSpawn : MonoBehaviour
 {
-    [SerializeField]
-    private SwipeEventAsset _swipeEventAsset;
-
-    DataManager _dataManager;
-
-    private void OnEnable()
-    {
-        _swipeEventAsset.eventRaised += FruitsRandomSpawn;
-    }
-
-    private void OnDisable()
-    {
-        _swipeEventAsset.eventRaised -= FruitsRandomSpawn;
-    }
-
+    // Managers.Data -> 작성하면 바로 사용 가능
     void FruitsRandomSpawn(object sender, SwipeData args)
     {
         if (Camera.main.transform.childCount == 1)
         {
             Debug.Log("던져짐");
             // 과일이 던져져 카메라 자식이 0일 때만 랜덤하게 과일 생성
-            int randomIndex = Random.Range(0, _dataManager.fruits.Count - 1); // 과일 목록에서 랜덤하게 인덱스 선택
-            Instantiate(_dataManager.fruits[randomIndex], transform.position, transform.rotation); // 생성 과일 위치 설정
+            int randomIndex = Random.Range(0, Managers.Data.fruits.Count - 1); // 과일 목록에서 랜덤하게 인덱스 선택
+
+            Instantiate(.fruits[randomIndex], transform.position, transform.rotation); // 생성 과일 위치 설정
         }
     }
 }
