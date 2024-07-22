ca3a2466395283ab273c2e9290172fa6e7eb5bf9
WoolimLee <woolimlee.dev@gmail.com>
2024-07-22 11:33:14 +0900

    AR-39 접시 설치기능 추가, inputAction 매니저 추가

--- Assets/Scripts/UI/Popup/UISetPlatePosition.cs
+++ Assets/Scripts/UI/Popup/UISetPlatePosition.cs
@@ -1,7 +1,13 @@
-using System;
+using Unity.VisualScripting;
+using Unity.XR.CoreUtils;
+using UnityEngine;
 
 public class UISetPlatePosition : UIPopup
 {
+    private GameObject _bowlPrefab;
+
+    private ARPlaceObject _arPlaceObject;
+
     enum Texts
     {
     }
@@ -17,14 +23,24 @@ public class UISetPlatePosition : UIPopup
             return false;
 
         BindButton(typeof(Buttons));
+        _arPlaceObject = FindObjectOfType<ARPlaceObject>();
 
         GetButton((int)Buttons.Install).gameObject.BindEvent(EnterToInGame);
+        _arPlaceObject.installable = true;
 
         return true;
     }
 
     private void EnterToInGame()
     {
+        // 월드상으로 설치된 접시가 없을 경우 다이얼로그 출력 후 그 이후의 로직 생략
+        if (_arPlaceObject.GetSpawnedGameObject() == null)
+        {
+            Managers.UI.ShowPopupUI<UIConfirmDialog>().SetDialog(() => { }, "Info", "Please add a bowl in world with touch.", "Confirm");
+            return;
+        }
+
+        _arPlaceObject.installable = false;
         Managers.UI.ClosePopupUI(this);
         Managers.UI.ShowPopupUI<UIInGame>();
     }
