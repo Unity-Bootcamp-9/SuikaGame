6335360f1ec74b7a64589eb91201a6c9b0c87b69
WoolimLee <woolimlee.dev@gmail.com>
2024-07-23 17:40:42 +0900

    AR-154 데이터 형식을 JSON형식으로 변경

--- Assets/Scripts/Manager/DataManager.cs
+++ Assets/Scripts/Manager/DataManager.cs
@@ -9,7 +9,7 @@ using UnityEngine;
 
 public class DataManager
 {
-    public List<FruitsData> fruits { get; private set; }
+    public FruitsData[] fruits { get; private set; }
     // NOTE : 데이터테이블 추가해야함.
     // NOTE : 느슨한 식별자의 경우 List를, 엄격한 식별자의 경우 Dictionary 사용.
     // NOTE : Id를 열거형으로 만들어두면 오류낼 일이 적음
@@ -18,33 +18,28 @@ public class DataManager
 
     public void Init()
     {
-        fruits = ParseToList<FruitsData>("FruitsData");
-
-        foreach (FruitsData fruits in fruits)
-        {
-            Debug.Log(fruits.name);
-        }
+        fruits = ParseToList<FruitGameData>("fruits").fruits;
     }
 
-    public List<T> ParseToList<T>([NotNull] string path)
+    public T ParseToList<T>([NotNull] string path)
     {
-        using (var reader = new StringReader(Resources.Load<TextAsset>($"CSV/{path}").text))
+        using (var reader = new StringReader(Resources.Load<TextAsset>($"Data/{path}").text))
         {
-            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
-            {
-                return csv.GetRecords<T>().ToList();
-            }
+            string json = reader.ReadToEnd();
+            T gameData = JsonUtility.FromJson<T>(json);
+
+            return gameData;
         }
     }
 
-    public Dictionary<Key, Item> ParseToDict<Key, Item>([NotNull] string path, Func<Item, Key> keySelector)
-    {
-        using (var reader = new StringReader(Resources.Load<TextAsset>($"CSV/{path}").text))
-        {
-            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
-            {
-                return csv.GetRecords<Item>().ToDictionary(keySelector);
-            }
-        }
-    }
+    //public Dictionary<Key, Item> ParseToDict<Key, Item>([NotNull] string path, Func<Item, Key> keySelector)
+    //{
+    //    using (var reader = new StringReader(Resources.Load<TextAsset>($"Data/{path}").text))
+    //    {
+    //        using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+    //        {
+    //            return csv.GetRecords<Item>().ToDictionary(keySelector);
+    //        }
+    //    }
+    //}
 }
