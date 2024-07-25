f249bb76a4b3b1167e6f6cdebc929dde7430b86c
YeonJangL <lej32552002@gmail.com>
2024-07-25 15:26:26 +0900

    AR-131 메인화면, 게임 오버 화면에 따라 버튼 활성화 / 비활성화 기능 수정

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -5,35 +5,40 @@ public class GameManager
 {
     public bool isGameOverDialogEnabled = false;
 
-    public void EnableMainDialog()
-    {
-        if (isGameOverDialogEnabled == false)
-        {
-            isGameOverDialogEnabled = true;
-            Managers.UI.ShowPopupUI<UIScoreBoard>().SetMainDialog(
-                ReloadGameScene,
-                "Score",
-                1, // Json으로 데이터 처리시 Num 내림 차순으로 수정
-                Managers.ScoreManager.Score, // 씬 바뀌면서 점수 초기화 되기 때문에 Json에 점수 저장 후 불러오는 로직 필요
-                "MainMenu"
-                );
-        }
-    }
-
     public void EnableGameOverDialog()
     {
         if (isGameOverDialogEnabled == false)
         {
             isGameOverDialogEnabled = true;
-            Managers.UI.ShowPopupUI<UIScoreBoard>().SetRestartDialog(
+            Managers.UI.ShowPopupUI<UIScoreBoard>().SetBoardDialog(
                 LoadMainMenu,
                 ReloadGameScene,
                 "Score",
                 1, // Json으로 데이터 처리시 Num 내림 차순으로 수정
-                Managers.ScoreManager.Score, // 가장 최근 게임 점수
+                $"{Managers.ScoreManager.Score}",
                 Managers.ScoreManager.Score, // 씬 바뀌면서 점수 초기화 되기 때문에 Json에 점수 저장 후 불러오는 로직 필요
                 "MainMenu",
-                "ReStart"
+                "ReStart",
+                false
+                );
+        }
+    }
+
+    public void EnableMainDialog()
+    {
+        if (isGameOverDialogEnabled == false)
+        {
+            isGameOverDialogEnabled = true;
+            Managers.UI.ShowPopupUI<UIScoreBoard>().SetBoardDialog(
+                LoadMainMenu,
+                null,
+                "Score",
+                1, // Json으로 데이터 처리시 Num 내림 차순으로 수정
+                $"",
+                Managers.ScoreManager.Score, // 씬 바뀌면서 점수 초기화 되기 때문에 Json에 점수 저장 후 불러오는 로직 필요
+                "MainMenu",
+                "Restart",
+                true
                 );
         }
     }
--- Assets/Scripts/UI/Popup/UIScoreBoard.cs
+++ Assets/Scripts/UI/Popup/UIScoreBoard.cs
@@ -1,5 +1,5 @@
 using System;
-using UnityEngine.SceneManagement;
+using UnityEngine;
 
 public class UIScoreBoard : UIPopup
 {
@@ -10,22 +10,23 @@ public class UIScoreBoard : UIPopup
         Number, // 오름 차순 번호
         //UserName, // 사용자 이름 -> 로컬 환경에서는 필요 없어서 우선 주석 처리함
         Score, // 내림 차순으로 정렬된 역대 점수
-        MainConfirmText, // 메인 버튼 텍스트
-        RestartConfirmText // 재시작 버튼 텍스트
+        MainButtonText, // 메인 버튼 텍스트
+        RestartButtonText // 재시작 버튼 텍스트
     }
 
     enum Buttons
     {
-        MainConfirm,
-        RestartConfirm
+        MainButton,
+        RestartButton
     }
 
     string _title;
     string _number;
     string _currentScore;
     string _score;
-    string _mainConfirm;
-    string _restartConfirm;
+    string _mainButton;
+    string _restartButton;
+    bool _isGameOver;
 
     public override bool Init()
     {
@@ -35,38 +36,36 @@ public class UIScoreBoard : UIPopup
         BindText(typeof(Texts));
         BindButton(typeof(Buttons));
 
-        GetButton((int)Buttons.MainConfirm).gameObject.BindEvent(OnClickMainButton);
-        GetButton((int)Buttons.RestartConfirm).gameObject.BindEvent(OnClickMainButton);
+        GetButton((int)Buttons.MainButton).gameObject.BindEvent(OnClickMainButton);
+        GetButton((int)Buttons.RestartButton).gameObject.BindEvent(OnClickRestartButton);
 
         GetText((int)Texts.TitleText).text = _title;
         GetText((int)Texts.Number).text = _number;
         GetText((int)Texts.CurrentText).text = _currentScore;
         GetText((int)Texts.Score).text = _score;
-        GetText((int)Texts.MainConfirmText).text = _mainConfirm;
-        GetText((int)Texts.RestartConfirmText).text = _restartConfirm;
+        GetText((int)Texts.MainButtonText).text = _mainButton;
+        GetText((int)Texts.RestartButtonText).text = _restartButton;
+
+        if (_isGameOver)
+        {
+            // 버튼 비활성화
+            GetButton((int)Buttons.RestartButton).gameObject.SetActive(false);
+        }
 
         return true;
     }
 
-    public void SetMainDialog(Action onClickMainButton, string title, int number, int score, string mainConfirm)
+    public void SetBoardDialog(Action onClickMainButton, Action? onClickRestartButton, string title, int number, string currentScore, int score, string mainButton, string restartButton, bool isGameOver)
     {
         _onClickMainButton = onClickMainButton;
-        _title = title;
-        _number = number.ToString();
-        _score = score.ToString();
-        _mainConfirm = mainConfirm.ToString();
-    }
-
-    public void SetRestartDialog(Action onClickMainButtonAction, Action onClickRestartButton, string title, int number, int currentScore, int score, string mainConfirm, string reStartConfirm)
-    {
-        _onClickMainButton = onClickRestartButton;
         _onClickRestartButton = onClickRestartButton;
         _title = title;
         _number = number.ToString();
-        _currentScore = currentScore.ToString();
+        _currentScore = currentScore.ToString(); // null 이면 restartButton 비활성화
         _score = score.ToString();
-        _mainConfirm = mainConfirm.ToString();
-        _restartConfirm = reStartConfirm.ToString();
+        _mainButton = mainButton.ToString();
+        _restartButton = restartButton.ToString();
+        _isGameOver = isGameOver;
     }
 
     Action _onClickMainButton;
